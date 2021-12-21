d6909ea21a777d4cf2bfa75a26980609aafe2ae7
Elvis <elvism72@gmail.com>
2021-12-21 10:28:10 +0100

    feat : [employe] create repository

---
+++ src/main/java/employes/EmployeRepository.java
@@ -0,0 +1,8 @@
+package employes;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeRepository extends JpaRepository<Employe,Long> {
+}
